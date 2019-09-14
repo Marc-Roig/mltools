28b00f8cd6614e2987ece26aae5f0bb3d2bd482f
Marc-Roig <marc12info@gmail.com>
2019-09-14 21:26:45 +0200

    first commit

---
+++ mltools/tf2/utils/gpu.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+
+
+def allow_memory_growth():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
+
+def block_memory_growth():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, False)
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
+
+def set_gpu_memory_limit(gpu, memory_limit):
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        # Restrict TensorFlow to only allocate specified GB of memory on GPU
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpus[gpu],
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)])
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Virtual devices must be set before GPUs have been initialized
+            print(e)
---
+++ setup_personal_utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mltools',
+    description='Suite of scripts for personal use',
+    version='0.0.1',
+    autor='Marc Roig',
+    packages=find_packages()
+)
